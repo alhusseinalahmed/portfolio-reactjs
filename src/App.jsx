45f91436c8970c8a91ab51333c517bb27f4ec39d
Al Hussein Al Ahmed <aa227hr@student.lnu.se>
2025-01-01 10:37:19 +0100

    fixed uppercase bug for About.Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import { Navbar } from "./components/navbar/Navbar.jsx";
 import { Hero } from "./components/hero/Hero.jsx";
-import { About } from "./components/about/About.Jsx";
+import { About } from "./components/about/About.jsx";
 import { Experience } from "./components/experience/Experience.jsx";
 import { Projects } from "./components/projects/Projects.jsx";
 import { Contact } from "./components/contact/Contact.jsx";
