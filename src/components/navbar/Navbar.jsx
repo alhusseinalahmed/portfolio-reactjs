e7274ae81dd1d5711c636fe8a63ba6db26957992
Al Hussein Al Ahmed <aa227hr@student.lnu.se>
2025-01-02 12:40:36 +0100

    minor visual improvments

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Portfolio
+        Al Hussein Al Ahmed
       </a>
       <div className={styles.menu}>
         <img
